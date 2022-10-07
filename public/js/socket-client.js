608074ca380f362f29067427254f8b4db1fdaba0
Diego Pagini <diego.pagini@neoris.com>
2022-10-07 08:23:12 +0200

    socket events

--- public/js/socket-client.js
+++ public/js/socket-client.js
@@ -1,3 +1,27 @@
 /** @format */
 
+// HTML references.
+const lblOnline = document.querySelector('#lblOnline');
+const lblOffline = document.querySelector('#lblOffline');
+
 const socket = io(); // To start connection with socket.
+
+/**
+ * connect event.
+ */
+socket.on('connect', () => {
+	console.log('Connected');
+
+	lblOffline.style.display = 'none';
+	lblOnline.style.display = 'inline-block';
+});
+
+/**
+ * disconnect event.
+ */
+socket.on('disconnect', () => {
+	console.log('Disconnected');
+
+	lblOffline.style.display = 'inline-block';
+	lblOnline.style.display = 'none';
+});
